9ad2a338aaedc6af7654993c03959928d089961d
evgeny.chernov <EvgenyCher89@gmail.com>
2025-12-27 14:21:02 +0300

    Упражнение по созданию 10 запросов паралельно через горутины

--- go-demo-1/main.go
+++ go-demo-1/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"time"
 )
 
@@ -9,7 +10,24 @@ func main() {
 
 	go printHi()
 	fmt.Println("Hello, World!2")
-	time.Sleep(1 * time.Second)
+
+	for i := 0; i < 10; i++ {
+		go printGoogle()
+	}
+
+	time.Sleep(2 * time.Second)
+}
+
+func printGoogle() {
+	t := time.Now()
+	resp, err := http.Get("https://www.google.com")
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer resp.Body.Close()
+	fmt.Println(resp.StatusCode)
+	fmt.Println(time.Since(t))
 }
 
 func printHi() {
