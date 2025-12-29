4b60517ccf87b8760f2fb62d0cf86b33ad6d1553
evgeny.chernov <EvgenyCher89@gmail.com>
2025-12-29 17:35:48 +0300

    запись данных в поток

--- go-demo-1/main.go
+++ go-demo-1/main.go
@@ -3,37 +3,22 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"sync"
-	"time"
 )
 
 func main() {
-	t := time.Now()
-	go printHi()
-	fmt.Println("Hello, World!2")
-	wg := sync.WaitGroup{}
-	for range 10 {
-		wg.Add(1)
-
-		go func() {
-			defer wg.Done()
-			printGoogle()
-		}()
-	}
-
-	wg.Wait()
-	fmt.Println(time.Since(t))
+	code := make(chan int)
+	go printGoogle(code)
+	fmt.Println(<-code)
 }
 
-func printGoogle() {
+func printGoogle(code chan int) {
 	resp, err := http.Get("https://www.google.com")
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
 	}
 	defer resp.Body.Close()
-	fmt.Println(resp.StatusCode)
-
+	code <- resp.StatusCode
 }
 
 func printHi() {
