d0e84a8ae8c64cc42c5dcc217897b2f6725057c6
evgeny.chernov <EvgenyCher89@gmail.com>
2026-01-01 15:44:18 +0300

    Создание файла конфигурации

--- go-http/cmd/main.go
+++ go-http/cmd/main.go
@@ -4,10 +4,13 @@ import (
 	"fmt"
 	"net/http"
 
+	"app/adv-http/configs"
 	"app/adv-http/internal/hello"
 )
 
 func main() {
+
+	config := configs.LoadConfig()
 	router := http.NewServeMux()
 	hello.NewHelloHandler(router)
 	fmt.Println("Server is running on port 8081")
