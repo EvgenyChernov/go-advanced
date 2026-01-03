4e13d9ac1f12b2a1e5f71c31dcb87b46360fb786
evgeny.chernov <EvgenyCher89@gmail.com>
2026-01-03 23:07:29 +0300

    создание простого логера

---
+++ go-http/pkg/middleware/common.go
@@ -0,0 +1,15 @@
+package middleware
+
+import (
+	"net/http"
+)
+
+type WrapperWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (w *WrapperWriter) WriteHeader(statusCode int) {
+	w.ResponseWriter.WriteHeader(statusCode)
+	w.statusCode = statusCode
+}
--- go-http/pkg/middleware/logs.go
+++ go-http/pkg/middleware/logs.go
@@ -1,13 +1,20 @@
 package middleware
 
 import (
-	"fmt"
+	"log"
 	"net/http"
+	"time"
 )
 
 func Logging(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println(r.Method, r.URL.Path)
-		next.ServeHTTP(w, r)
+		startTime := time.Now()
+		wrapper := &WrapperWriter{
+			ResponseWriter: w,
+			statusCode:     http.StatusOK,
+		}
+		next.ServeHTTP(wrapper, r)
+		duration := time.Since(startTime)
+		log.Printf("%s %s %s %d %s\n", r.Method, r.URL.Path, r.Proto, wrapper.statusCode, duration)
 	})
 }
