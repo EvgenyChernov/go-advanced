64c83602c862a8913596581b552181522a7b36b6
evgeny.chernov <EvgenyCher89@gmail.com>
2026-01-06 10:58:00 +0300

    продвинутые запросы

--- go-http/internal/link/repository.go
+++ go-http/internal/link/repository.go
@@ -60,11 +60,17 @@ func (repo *LinkRepository) GetById(id uint) (*Link, error) {
 	return &link, nil
 }
 
-func (repo *LinkRepository) GetLinks(limit, offset int) []Link {
+func (repo *LinkRepository) Count() int64 {
+	var count int64
+	repo.Database.DB.Table("links").Where("deleted_at is null").Count(&count)
+	return count
+}
+
+func (repo *LinkRepository) GetAll(limit, offset int) []Link {
 	var links []Link
 	repo.Database.
 		Table("links").
-		Where("deleted_at IS NULL").
+		Where("deleted_at is null").
 		Order("id asc").
 		Limit(limit).
 		Offset(offset).
